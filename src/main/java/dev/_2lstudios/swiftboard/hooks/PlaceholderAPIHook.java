7928200bdbe672d779aa6860765f1209ddcf0bfc
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-10-13 15:16:01 -0300

    not null check

--- src/main/java/dev/_2lstudios/swiftboard/hooks/PlaceholderAPIHook.java
+++ src/main/java/dev/_2lstudios/swiftboard/hooks/PlaceholderAPIHook.java
@@ -7,8 +7,10 @@ import me.clip.placeholderapi.PlaceholderAPI;
 
 public class PlaceholderAPIHook {
     public static String setPlaceholders(final OfflinePlayer player, final String text) {
-        if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
-            return PlaceholderAPI.setPlaceholders(player, text);
+        if (text != null) {
+            if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+                return PlaceholderAPI.setPlaceholders(player, text);
+            }
         }
 
         return text;
